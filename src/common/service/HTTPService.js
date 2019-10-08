7b2e7ed6a4be0116c7ee7a72ff8c9a7c0cf02afc
Your Name <you@example.com>
2019-10-08 16:26:24 +0530

    Htpp Service Setup

---
+++ src/common/service/HTTPService.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import { BASE_URL } from '../constants/variables';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// axiosInstance.interceptors.request.use((config) => {
+//   config.headers["user-key"] = localStorage.getItem('accessToken') || '';
+//   return config
+// })
+
+export class HTTPService {
+
+  static get(url, params) {
+    return new Promise((resolve, reject) => {
+      axiosInstance.get(url, { params: params })
+        .then(response => {
+          if (response.status === 200)
+            resolve(response.data)
+        })
+        .catch((error) => reject(error))
+    })
+  }
+
+  static put(url, body) {
+    return new Promise((resolve, reject) => {
+      axiosInstance.put(url, body)
+        .then(response => {
+          if (response.status === 200)
+            resolve(response.data)
+        })
+        .catch((error) => reject(error))
+    })
+  }
+
+  static post(url, body) {
+    return new Promise((resolve, reject) => {      
+      axiosInstance.post(url, body)
+        .then(response => {
+          if (response.status === 200) {
+            resolve(response.data)
+          }
+        })
+        .catch((error) => reject(error))
+    })
+  }
+
+  static delete(url) {
+    return new Promise((resolve, reject) => {
+      axiosInstance.delete(url)
+        .then(response => {
+          if (response.status === 200) {
+            resolve(response.data)
+          }
+        })
+        .catch((error) => reject(error))
+    })
+  }
+}
+
+// function saveToken(reponse) {
+//   if (reponse.data && reponse.data.accessToken) {
+//     localStorage.setItem('accessToken', reponse.data.accessToken)
+//   }
+// }
